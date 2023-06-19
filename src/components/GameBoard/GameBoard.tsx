1d70b1a68d9a03c8f20e3919c126b7253792672e
Felicia <feelicianorman@hotmail.com>
2023-06-19 21:20:33 +0200

    [build] hid front cards

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -9,35 +9,26 @@ import dog5 from "../../assets/dog5.png";
 import dog6 from "../../assets/dog6.png";
 
 export const GameBoard = () => {
-  const frontCards: Game[] = [
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
-    { img: front, name: "front", flipped: false },
+
+
+  const cards: Game[] = [
+    { img: dog1, name: "dog1", flipped: false, id: Math.random() },
+    { img: dog1, name: "dog1", flipped: false, id: Math.random() },
+    { img: dog2, name: "dog2", flipped: false, id: Math.random()  },
+    { img: dog2, name: "dog2", flipped: false , id: Math.random() },
+    { img: dog3, name: "dog3", flipped: false , id: Math.random() },
+    { img: dog3, name: "dog3", flipped: false, id: Math.random()  },
+    { img: dog4, name: "dog4", flipped: false, id: Math.random()  },
+    { img: dog4, name: "dog4", flipped: false, id: Math.random()  },
+    { img: dog5, name: "dog5", flipped: false , id: Math.random() },
+    { img: dog5, name: "dog5", flipped: false , id: Math.random() },
+    { img: dog6, name: "dog5", flipped: false , id: Math.random() },
+    { img: dog6, name: "dog5", flipped: false, id: Math.random()  },
   ];
 
-  const backCards: Game[] = [
-    { img: dog1, name: "dog1", flipped: false },
-    { img: dog1, name: "dog1", flipped: false },
-    { img: dog2, name: "dog2", flipped: false },
-    { img: dog2, name: "dog2", flipped: false },
-    { img: dog3, name: "dog3", flipped: false },
-    { img: dog3, name: "dog3", flipped: false },
-    { img: dog4, name: "dog4", flipped: false },
-    { img: dog4, name: "dog4", flipped: false },
-    { img: dog5, name: "dog5", flipped: false },
-    { img: dog5, name: "dog5", flipped: false },
-    { img: dog6, name: "dog5", flipped: false },
-    { img: dog6, name: "dog5", flipped: false },
-  ];
+  const handleClick = (name: string) => {
+    console.log('klickad', name)
+  }
 
   return (
     <>
@@ -47,12 +38,11 @@ export const GameBoard = () => {
           <h2>Poäng: 0</h2> <h2>Tid: 0 s</h2>
         </div>
         <div className="game">
-          {frontCards.map((front) => (
-            
-            <img src={front.img} className="frontCard" />
-          ))}
-          {backCards.map((back) => (
-            <img src={back.img} className="backCard" />
+          {cards.map((card) => (
+            <div className="card" key={card.id} onClick={() => handleClick(card.name)}>
+              <img className="backCard" src={card.img} />
+              <img className="frontCard" src={front} />
+            </div>
           ))}
         </div>
       </div>
