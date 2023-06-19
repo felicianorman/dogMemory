aa7e1d177779898fe65035504d71b2b30b38cbef
Felicia <feelicianorman@hotmail.com>
2023-06-19 17:52:48 +0200

    [build] started building dog cards

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,6 +1,12 @@
 import { Game } from "../../models/Game";
 import "./GameBoard.scss";
 import front from "../../assets/front.png";
+import dog1 from "../../assets/dog1.png";
+import dog2 from "../../assets/dog2.png";
+import dog3 from "../../assets/dog3.png";
+import dog4 from "../../assets/dog4.png";
+import dog5 from "../../assets/dog5.png";
+import dog6 from "../../assets/dog6.png";
 
 export const GameBoard = () => {
   const frontCards: Game[] = [
@@ -18,6 +24,21 @@ export const GameBoard = () => {
     { img: front, name: "front", flipped: false },
   ];
 
+  const backCards: Game[] = [
+    { img: dog1, name: "dog1", flipped: false },
+    { img: dog1, name: "dog1", flipped: false },
+    { img: dog2, name: "dog2", flipped: false },
+    { img: dog2, name: "dog2", flipped: false },
+    { img: dog3, name: "dog3", flipped: false },
+    { img: dog3, name: "dog3", flipped: false },
+    { img: dog4, name: "dog4", flipped: false },
+    { img: dog4, name: "dog4", flipped: false },
+    { img: dog5, name: "dog5", flipped: false },
+    { img: dog5, name: "dog5", flipped: false },
+    { img: dog6, name: "dog5", flipped: false },
+    { img: dog6, name: "dog5", flipped: false },
+  ];
+
   return (
     <>
       <div className="game--wrapper">
@@ -27,7 +48,11 @@ export const GameBoard = () => {
         </div>
         <div className="game">
           {frontCards.map((front) => (
+            
             <img src={front.img} className="frontCard" />
+          ))}
+          {backCards.map((back) => (
+            <img src={back.img} className="backCard" />
           ))}
         </div>
       </div>
